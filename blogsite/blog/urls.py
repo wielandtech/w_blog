ce98666221e05d46ad3709235ee545193929b5a8
Raphael Wieland <raphael@wielandtech.com>
2022-04-26 00:45:14 -0600

    refactored 'post_list' to make use of base 'ListView' class

--- blogsite/blog/urls.py
+++ blogsite/blog/urls.py
@@ -5,6 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     # Post views
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
